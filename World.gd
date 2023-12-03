90a3cb99ad8d73f3354aa7b3ddbf97e428bc2fae
LuisSilva-dev <lcd.silva@campus.fct.unl.pt>
2023-12-03 20:25:36 +0000

    Added handBook and talkBubbles

--- World.gd
+++ World.gd
@@ -17,14 +17,14 @@ func _process(delta):
 
 
 func _potion_box_clicked(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT and isOpen and !hasIngredient:
 		print("I got the potion from the box")
 		hasPotion = true
 		
 
 
 func _alchemist_setup_clicked(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT and isOpen:
 		if hasPotion:
 			print("I clicked the setup with potion")
 			hasPotion = false
@@ -36,8 +36,20 @@ func _alchemist_setup_clicked(viewport, event, shape_idx):
 
 
 func _ingredient_box_clicked(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT and isOpen and !hasPotion:
 		print("I clicked the ingredient box")
 		hasIngredient = true
 		# add ingredient to setup
 		
+
+func _open_sign_clicked(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		print("sign clicked")
+		isOpen = true
+		get_node("OpenSign/Sign").text = "Open"
+
+
+func _hand_book_clicked(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		print("book clicked")
+		# display popup instructions
